026fcce6f554b799fdb43239c640ed494b5b62f6
mamadkiller2 <mamadkillergg@gmail.com>
2016-12-17 14:38:13 +0330

    Update clean_delete_ac.lua

--- plugins/clean_delete_ac.lua
+++ plugins/clean_delete_ac.lua
@@ -3,7 +3,7 @@ local receiver = cb_extra.receiver
  local msg = cb_extra.msg
   local deleted = 0 
 if success == 0 then
-send_large_msg(receiver, "first set me as admin!") 
+send_large_msg(receiver, "اجازه چنین کاری ندارم . مرا ادمین کن") 
 end
 for k,v in pairs(result) do
   if not v.first_name and not v.last_name then
@@ -11,7 +11,7 @@ deleted = deleted + 1
  kick_user(v.peer_id,msg.to.id)
  end
  end
- send_large_msg(receiver, deleted.." Deleted account removed from group!") 
+ send_large_msg(receiver, deleted.." کاربرانی که دلیت اکانت زده اند حذف شدند") 
  end 
 local function run(msg, matches)
 if is_owner(msg) then
@@ -22,6 +22,7 @@ channel_get_users(receiver, check_member_super_deleted,{receiver = receiver, msg
  return {
        patterns = {
     "^[/#!](clean delacc)$"
+   "^[/#!](پاک کن)$"
  },
 run = run,
 }

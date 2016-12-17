5eb19de63e927494096897fad04eaf327a8d7283
mamadkiller2 <mamadkillergg@gmail.com>
2016-12-17 14:48:50 +0330

    Update expire.lua

--- plugins/expire.lua
+++ plugins/expire.lua
@@ -37,39 +37,39 @@ local function pre_process(msg)
 		if msg.text:match('/') then
 		rem_mutes(msg.to.id)
 		superrem2(msg)
-		return send_large_msg(get_receiver(msg), '<i>✨تاریخ اتقضای گروه به پایان رسید.✨</i>\nبرای تمدید از سایت http://Tarfand.pro یا ربات @NECCBOT اقدام کنید✨')
+		return send_large_msg(get_receiver(msg), '<i>✨تاریخ اتقضای گروه به پایان رسید.✨</i>\nf✨')
 		else
 			return
 		end
 	end
 	if tonumber(timetoexpire) == 0 then
 		if redis:hget('expires0',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '<i>✨0 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ربات جهت تمدید: @NECCBOT✨\n✨سایت جهت تمدید: Tarfand.pro✨</i>')
+		send_large_msg(get_receiver(msg), '<i>✨0 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ایدی جهت تمدید: @mamadkiller✨</i>')
 		redis:hset('expires0',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 1 then
 		if redis:hget('expires1',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '<i>✨1 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ربات جهت تمدید: @NECCBOT✨\n✨سایت جهت تمدید: Tarfand.pro✨t</i>')
+		send_large_msg(get_receiver(msg), '<i>✨1 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ایدی جهت تمدید: @mamadkiller✨</i>')
 		redis:hset('expires1',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 2 then
 		if redis:hget('expires2',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '<i>✨2 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ربات جهت تمدید: @NECCBOT✨\n✨سایت جهت تمدید: Tarfand.pro✨</i>')
+		send_large_msg(get_receiver(msg), '<i>✨2 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ایدی جهت تمدید: @mamadkiller✨</i>')
 		redis:hset('expires2',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 3 then
 		if redis:hget('expires3',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '<i>✨3 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ربات جهت تمدید: @NECCBOT✨\n✨سایت جهت تمدید: Tarfand.pro✨</i>')
+		send_large_msg(get_receiver(msg), '<i>✨3 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ایدی جهت تمدید: @mamadkiller✨</i>')
 		redis:hset('expires3',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 4 then
 		if redis:hget('expires4',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '<i>✨4 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ربات جهت تمدید: @NECCBOT✨\n✨سایت جهت تمدید: Tarfand.pro✨</i>')
+		send_large_msg(get_receiver(msg), '<i>✨4 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ایدی جهت تمدید: @mamadkiller✨</i>')
 		redis:hset('expires4',msg.to.id,'5')
 	end
 	if tonumber(timetoexpire) == 5 then
 		if redis:hget('expires5',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '<i>✨5 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ربات جهت تمدید: @NECCBOT✨\n✨سایت جهت تمدید: Tarfand.pro✨</i>')
+		send_large_msg(get_receiver(msg), '<i>✨5 روز تا پایان تاریخ انقضای گروه باقی مانده است✨\n✨نسبت به تمدید اقدام کنید✨\n✨ایدی جهت تمدید: @mamadkiller✨\n✨ </i>')
 		redis:hset('expires5',msg.to.id,'5')
 	end
 end
@@ -82,7 +82,7 @@ function run(msg, matches)
 		local buytime = tonumber(os.time())
 		local timeexpire = tonumber(buytime) + (tonumber(matches[2]) * 86400)
 		redis:hset('expiretime',get_receiver(msg),timeexpire)
-		return "<i>✨تاریخ انقضای گروه✨:\n✨به "..matches[2].. " روز دیگر تنظیم شد.\nسایت رسمی:Tarfand.pro✨</i>"
+		return "<i>✨تاریخ انقضای گروه✨:\n✨به "..matches[2].. " روز دیگر تنظیم شد.\n ایدی : @mamadkiller ✨</i>"
 	end
 	if matches[1]:lower() == 'expire' then
 		local expiretime = redis:hget ('expiretime', get_receiver(msg))

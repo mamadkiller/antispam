3224e5396d530b5283a715dd43fdd3ce0f951805
mamadkiller2 <mamadkillergg@gmail.com>
2016-12-20 23:21:36 +0330

    Update and rename dlpl.lua to deleteplugin.lua

--- plugins/deleteplugin.lua
+++ plugins/deleteplugin.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
     if matches[1] == "delplugin" and is_sudo(msg) then
 text = io.popen("cd plugins && rm "..matches[2]..".lua")
-return " پلاگین ["..matches[2].."] توسط ["..msg.from.id.."] با موفقست حذف شد"
+return " پلاگین ["..matches[2].."] توسط ["..msg.from.id.."] .با موفقیت حذف شد"
 end 
 end
 return { 
